bab5a9bf81cc0764c0968455cac9018b513d2ef6
Saarth Shah <69341449+SaarthShah@users.noreply.github.com>
2021-04-21 23:07:57 +0530

    Update excelautomator.py

--- excelautomator.py
+++ excelautomator.py
@@ -1,16 +1,18 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
-wb= xl.load_workbook('transactions.xlsx')
-sheet= wb['Sheet1']
 
-for row in range(2,sheet.max_row+1):
-    corrected_price= sheet.cell(row,3).value*0.9
-    corrected_price_cell=sheet.cell(row,4)
-    corrected_price_cell.value= corrected_price
+def process_wb(filename):
+    wb= xl.load_workbook(filename)
+    sheet= wb['Sheet1']
 
-values= Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=4, max_col=4)
+    for row in range(2,sheet.max_row+1):
+        corrected_price= sheet.cell(row,3).value*0.9
+        corrected_price_cell=sheet.cell(row,4)
+        corrected_price_cell.value= corrected_price
 
-chart= BarChart()
-chart.add_data(values)
-sheet.add_chart(chart,'e2')
-wb.save('transactions2.xlsx')
+    values= Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=4, max_col=4)
+
+    chart= BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart,'e2')
+    wb.save(filename)
